ad7c9bd99696a8e2a6a1a65ffcb7501e82a03714
Lea Verou <lea@verou.me>
2013-05-23 22:27:25 +0300

    Removed special casing for MW TOC, increased the heading count threshold to 2, moved TOC into gutter

--- skins/webplatform/webplatform.js
+++ skins/webplatform/webplatform.js
@@ -64,19 +64,13 @@ $(document).ready(function(){
 	var page = document.getElementById('mw-content-text');
 	var headings = Array.prototype.slice.apply(page.querySelectorAll('h2, h3, h4, h5, h6'));
 	
-	if (!headings.length) {
+	if (headings.length < 2) {
 		return;
 	}
 	
 	var ol = document.createElement('ol'), li, rootOl = ol;
 	
-	var exclude = document.querySelector('#toc h2');
-	
 	for (var i=0, h; h=headings[i++];) {
-		if (h == exclude) {
-			continue;
-		}
-		
 		var level = hLevel(h);
 		
 		if (level > previousLevel) {
@@ -146,5 +140,6 @@ $(document).ready(function(){
 	toc.appendChild(tocH);
 	toc.appendChild(rootOl);
 	
-	page.insertBefore(toc, page.firstChild);
+	page.parentNode.insertBefore(toc, page);
+	page.style.marginRight = toc.offsetWidth + 'px';
 });
