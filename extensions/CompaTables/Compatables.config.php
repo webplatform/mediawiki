bb58d4d71b1ffd5b029e7420cc67b142c27b01e4
Aaron Schulz <aschulz@wikimedia.org>
2013-10-26 16:37:07 -0700

    Split out Compatables.class.php file
    * Also added missing config file

---
+++ extensions/CompaTables/Compatables.config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Whether to use Edge-Side-Includes for tables */
+$wgCompatablesUseESI = false;
+
+$wgAvailableRights[] = 'purgecompatables';
+
+$wgGroupPermissions['sysop']['purgecompatables'] = true;
