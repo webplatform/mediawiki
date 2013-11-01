f7d90bf690f34c7cb7f74f310f4eee9dde43497a
Renoir Boulanger <hello@renoirboulanger.com>
2013-11-01 11:14:17 -0400

    Syncing from production

--- skins/WebPlatform.php
+++ skins/WebPlatform.php
@@ -303,18 +303,6 @@ class WebPlatformTemplate extends BaseTemplate {
 		</footer>
 		<!-- /footer -->
 		<?php $this->printTrail(); ?>
-<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.webplatform.org/track/" : "http://stats.webplatform.org/track/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://stats.webplatform.org/track/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Tracking Code -->
 	</body>
 	</html>
 <?php
